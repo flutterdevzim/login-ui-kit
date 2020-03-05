93f8291ac6dbca7659b919afd6c437d8560dcb5e
Alistair Holmes <alistairholmes95@gmail.com>
2020-03-05 23:04:38 +0200

    init commit :baby:

---
+++ android/app/src/main/kotlin/loginuikit/alistairholmes/github/io/login_ui_kit/MainActivity.kt
@@ -0,0 +1,12 @@
+package loginuikit.alistairholmes.github.io.login_ui_kit
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/register.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  String email;
+  String password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            'images/alto.png',
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            top: 100,
+            left: 70,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Travelers',
+                    style: TextStyle(
+                        fontFamily: 'Mermaid',
+                        fontSize: 55,
+                        //fontWeight: FontWeight.w700,
+                        color: Colors.black),
+                  ),
+                  Text(
+                    'Travel Community App',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 19,
+                        //fontWeight: FontWeight.w700,
+                        color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 200,
+            left: 50,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 70,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: Color(0xff978AA9).withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.mail_outline,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'Email Address',
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: 70,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: Color(0xff978AA9).withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.lock_outline,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'Password',
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              height: 70,
+              width: double.infinity,
+              child: FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Create new account',
+                  style: TextStyle(
+                      color: Colors.white, fontFamily: 'Poppins', fontSize: 20),
+                ),
+                color: Color(0xff474B7A),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
